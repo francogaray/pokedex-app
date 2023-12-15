b69964dfa7ccfe140e4b1d5ed4f12ad97219875c
francogaray <francogaray.dev@gmail.com>
2023-12-15 17:54:52 -0300

    remove & add favorites

--- src/components/Pokemon/Favorite.js
+++ src/components/Pokemon/Favorite.js
@@ -1,15 +1,58 @@
-import React from "react";
-import Icon from "react-native-vector-icons/FontAwesome5";
+import { set } from "lodash";
+import React, { useEffect, useState } from "react";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import {
+    isPokemonFavoriteApi,
+    addPokemonFavoriteApi,
+    removePokemonFavoriteApi,
+} from "../../api/favorite";
 
 const Favorite = (props) => {
-    const {id} = props;
-    const addFavorite = () => console.log("Adding Favorite", id);
+    const { id } = props;
+    const [isFavorite, setIsFavorite] = useState(undefined);
+    const Icon = isFavorite ? FontAwesome : FontAwesome5;
+    const [reloadCheck, setReloadCheck] = useState();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await isPokemonFavoriteApi(id);
+                setIsFavorite(response);
+            } catch (error) {
+                setIsFavorite(false);
+            }
+        })();
+    }, [id, reloadCheck]);
+
+    const onReloadCheck = () => {
+        setReloadCheck((prev) => !prev);
+    };
+
+    const addFavorite = async () => {
+        try {
+            await addPokemonFavoriteApi(id);
+            onReloadCheck();
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    const removeFavorite = async () => {
+        try {
+            await removePokemonFavoriteApi(id);
+            onReloadCheck();
+        } catch (error) {
+            throw error;
+        }
+    };
+
     return (
         <Icon
             name="heart"
             color="#fff"
             size={20}
-            onPress={addFavorite}
+            onPress={isFavorite ? removeFavorite : addFavorite}
             style={{ marginBottom: 20 }}
         />
     );
--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView, View, Text } from "react-native";
+import React from "react";
+import { Button } from "react-native";
+import { getPokemonsFavoritesApi } from "../api/favorite";
 
 export default function Favorite() {
-  return (
-    <View>
-      <Text>Favorite</Text>
-    </View>
-  )
+    const checkFavorites = async () => {
+        const response = await getPokemonsFavoritesApi();
+        console.log("response Screen Fav", response);
+    };
+
+    return (
+        <SafeAreaView>
+            <Text>Favorite</Text>
+            <Button title="Obtener Favoritos" onPress={checkFavorites} />
+        </SafeAreaView>
+    );
 }
